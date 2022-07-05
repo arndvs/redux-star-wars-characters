386a0683dfb4778cbd29b7a238afaa86e95df533
Aaron Davis <108478438+arndvs@users.noreply.github.com>
2022-07-05 15:57:16 -0700

    createAsyncThunk to search star wars name via API

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,9 @@
+// https://swapi.dev/search/
 const Character = ({ character }) => {
   const { id, name } = character;
   return (
     <article className="Character">
-      <a href={`https://star-wars-characters.glitch.me/api/characters/${id}`}>
+      <a href={`https://swapi.dev/api/people/${id}`}>
         {name}
       </a>
     </article>
